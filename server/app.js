c802d80c30da1db187397b8d833c89103e41d9ee
codecocos <isawmy@nate.com>
2021-12-19 21:56:52 +0900

    서버 포트 번호 5000 번으로 변경

--- server/app.js
+++ server/app.js
@@ -15,8 +15,8 @@ const maria = require('./maria');
 maria.connect();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -42,6 +42,7 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  //console.log(err);
 });
 
 module.exports = app;
